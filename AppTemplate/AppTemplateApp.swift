757b1f23355584716f503759b122e266455f77e7
pieterjooste <pieter.jooste.jp@gmail.com>
2024-05-07 13:05:30 +0200

    Initial Commit

---
+++ AppTemplate/AppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTemplateApp.swift
+//  AppTemplate
+//
+//  Created by Pieter Jooste on 2024/05/07.
+//
+
+import SwiftUI
+
+@main
+struct AppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppTemplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppTemplate
+//
+//  Created by Pieter Jooste on 2024/05/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
